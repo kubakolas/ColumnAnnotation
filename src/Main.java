72f87de582a9c17e8f63860c9a8fc204e525ff0d
Kuba Kolas <kubakolas12@gmail.com>
2019-12-08 13:23:22 +0100

    Init project

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Model;
+
+public class Main {
+    public static void main(String args[]) {
+
+        Model model = ModelFactory.createDefaultModel();
+    }
+}
